75d4987eb25a5e9ac8442edf1029f4140ed97eac
Damjan Miklic <damjan.miklic@gmail.com>
2016-05-17 09:05:23 +0200

    Adding first files.

---
+++ audio/download-audio-files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Download the appropriate audio files
+
+echo "This script is currently not working. Plase go to http://larics.rasip.fer.hr/laricscloud/index.php/s/rDyWRgQ4ycwHYyJ and download the files manually, keeping the directory structure."
